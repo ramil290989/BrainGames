b93492d2e3a79179a5124b4e0d00221f9865e27c
Ramil Garifullin <garifullinramil290989@gmail.com>
2022-10-18 01:26:37 +0500

    fix user name

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,8 @@ import readlineSync from 'readline-sync';
 
 const user = () => {
     console.log('Welcome to the Brain Games!');
-    var userName = readlineSync.question(`May I have your name? `);
-    console.log('Hello ' + userName + '!');
+    const userName = readlineSync.question(`May I have your name? `);
+    console.log(`Hello ${userName}!`);
     return userName;
 }
 
