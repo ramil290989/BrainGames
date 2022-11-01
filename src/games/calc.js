f8d97d5eb81372e9fd3684f9731fc8267ebc3c62
Ramil Garifullin <garifullinramil290989@gmail.com>
2022-11-02 02:43:14 +0500

    fix small errors

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,9 +5,9 @@ import getRandomNumber from '../utils.js';
 const mission = 'What is the result of the expression?';
 const randomRange = 10;
 
-const getCorrectAnswer = (expressionsItem, number1, number2) => {
+const getCorrectAnswer = (operator, number1, number2) => {
   let result = 0;
-  switch (expressionsItem) {
+  switch (operator) {
     case '+':
       result = number1 + number2;
       break;
@@ -26,8 +26,9 @@ const getQuestionAndCorrectAnswer = () => {
   const number2 = getRandomNumber(randomRange);
   const operatorsStr = '+-*';
   const expressionsItem = getRandomNumber(operatorsStr.length);
-  const correctAnswer = (getCorrectAnswer(expressionsItem, number1, number2)).toString();
-  const question = `${number1} ${operatorsStr[expressionsItem]} ${number2}`;
+  const operator = operatorsStr[expressionsItem];
+  const correctAnswer = (getCorrectAnswer(operator, number1, number2)).toString();
+  const question = `${number1} ${operator} ${number2}`;
   return [question, correctAnswer];
 };
 
