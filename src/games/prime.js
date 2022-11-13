9bdcb76ee60504605ae1379633fee8f242c2bb65
Ramil Garifullin <garifullinramil290989@gmail.com>
2022-11-13 21:09:40 +0500

    fix errors

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,11 +5,14 @@ const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 const randomRange = 100;
 
 const isPrime = (number) => {
-  if ((number === 1) || (number === 2)) {
+  if (number === 1) {
     return false;
   }
+  if (number === 2) {
+    return true;
+  }
   for (let i = 2; i < number; i += 1) {
-    if ((number % i) === 0) { 
+    if ((number % i) === 0) {
       return false;
       }
   }
